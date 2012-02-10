f675fdb42e5a3c1d002698e72310687a443a6b3f
Ben Phillips <ben@ben-phillips.net>
2012-02-10 23:18:26 +0000

    Initial untested MapReduce job.

---
+++ job.py
@@ -0,0 +1,15 @@
+from mrjob.job import MRJob
+
+class Calculator(MRJob):
+    def mapper(self, node):
+        yield node.region, node
+
+        for region in node.region.children:
+            yield region, node
+
+    def reducer(self, region, dates):
+        sort_key = lambda date: date.weight
+        yield region, max(sorted(dates, key=sort_key))
+
+if __name__ == '__main__':
+    Calculator.run()
