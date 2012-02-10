2504f62145b0eda15acccf7e76d877883f36394a
Ben Phillips <ben@ben-phillips.net>
2012-02-10 23:41:34 +0000

    Use dates not datetimes.

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ if __name__ == "__main__":
     game = Game('Super Mental 2')
 
     dates = [
-        ReleaseDate(game, datetime.datetime(2012, 1, 1), regions['global']),
-        ReleaseDate(game, datetime.datetime(2012, 1, 2), regions['asia']),
-        ReleaseDate(game, datetime.datetime(2012, 1, 3), regions['europe']),
-        ReleaseDate(game, datetime.datetime(2012, 1, 4), regions['china']),
-        ReleaseDate(game, datetime.datetime(2012, 1, 5), regions['britain']),
+        ReleaseDate(game, datetime.date(2012, 1, 1), regions['global']),
+        ReleaseDate(game, datetime.date(2012, 1, 2), regions['asia']),
+        ReleaseDate(game, datetime.date(2012, 1, 3), regions['europe']),
+        ReleaseDate(game, datetime.date(2012, 1, 4), regions['china']),
+        ReleaseDate(game, datetime.date(2012, 1, 5), regions['britain']),
     ]
 
     Calculator.run(dates)
