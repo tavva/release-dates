e11646f54564b8f55b3fc860facbf685a9e8e1e1
Ben Phillips <ben@ben-phillips.net>
2012-02-10 23:50:50 +0000

    Move input generation to own module.

---
+++ get_input.py
@@ -0,0 +1,24 @@
+import datetime
+
+from classes import Game, ReleaseDate, regions
+
+def generate_input_file(release_dates):
+    for release_date in release_dates:
+        print "%s,%s,%i" % (
+            release_date.date,
+            release_date.region.name,
+            release_date.region.weight,
+        )
+
+if __name__ == "__main__":
+    game = Game('Super Mental 2')
+
+    release_dates = [
+        ReleaseDate(game, datetime.date(2012, 1, 1), regions['global']),
+        ReleaseDate(game, datetime.date(2012, 1, 2), regions['asia']),
+        ReleaseDate(game, datetime.date(2012, 1, 3), regions['europe']),
+        ReleaseDate(game, datetime.date(2012, 1, 4), regions['china']),
+        ReleaseDate(game, datetime.date(2012, 1, 5), regions['britain']),
+    ]
+
+    generate_input_file(release_dates)
