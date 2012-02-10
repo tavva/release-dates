5ce26e05a23f73a3ada73c3ee24d1ce4f3f56b35
Ben Phillips <ben@ben-phillips.net>
2012-02-10 23:47:50 +0000

    Use raw input.

--- job.py
+++ job.py
@@ -1,12 +1,18 @@
 from mrjob.job import MRJob
 
-class Calculator(MRJob):
-    def mapper(self, node):
-        yield node.region, node
+from classes import regions
 
-        for region in node.region.children:
-            yield region, node
+class Calculator(MRJob):
+    def mapper(self, key, line):
+        date, region_name, weight = line.split(',')
+
+        region = regions[region_name]
+
+        yield region, (date, weight)
+
+        for region_name in region.children:
+            yield regions[region_name], (date, weight)
 
     def reducer(self, region, dates):
-        sort_key = lambda date: date.weight
+        sort_key = lambda date: date[1]
         yield region, max(sorted(dates, key=sort_key))
